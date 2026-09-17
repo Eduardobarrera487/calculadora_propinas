05fafd4872279450d618f5404d4d534da274f385
Eduardobarrera487 <barreraeduardo099@gmail.com>
2026-09-17 17:05:33 -0600

    primeros cambios y subida de datos

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5 ">
+        <h1 className="text-2xl font-bold text-white text-center">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
